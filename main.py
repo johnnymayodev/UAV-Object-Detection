eb381429d46a2f0d878cdb0317d3453868f92daa
Johnny <jmnaze123@gmail.com>
2023-12-05 16:39:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,70 @@
 import libraries.create as create
 
-try: 
+try:
     if __name__ == "__main__":
-
         print("welcome to the drone object detector!")
-        selection = input("\nwhat would you like to do?\n1. create a new model\n2. train an existing model\n3. test an existing model\n4. exit\n")
-
+        selection = input(
+            "\nwhat would you like to do?\n1. create a new model\n2. train an existing model\n3. test an existing model\n4. exit\n"
+        )
 
         if selection == "1":
-
-            selection = input("\nwould you like to set the model's parameters manually? (y/n)\n")
-
+            selection = input(
+                "\nwould you like to set the model's parameters manually? (y/n)\n"
+            )
 
             if selection == "y":
-
                 epochs = int(input("\nhow many epochs would you like to train for?\n"))
-                if epochs <= 0: raise ValueError 
-                
+                if epochs <= 0:
+                    raise ValueError
+
                 if epochs > 500:
-                    selection = input("WARNING: training for more than 500 epochs may cause overfitting.\nwould you like to continue? (y/n)\nchoice: ")
+                    selection = input(
+                        "WARNING: training for more than 500 epochs may cause overfitting.\nwould you like to continue? (y/n)\nchoice: "
+                    )
                     if selection == "n":
                         exit()
 
                 batch_size = int(input("what batch size would you like to use?\n"))
-                if batch_size < 1: raise ValueError
+                if batch_size < 1:
+                    raise ValueError
 
-                learning_rate = float(input("what learning rate would you like to use? (0 - 1)\n"))
-                if learning_rate < 0 or learning_rate > 1: raise ValueError
+                learning_rate = float(
+                    input("what learning rate would you like to use? (0 - 1)\n")
+                )
+                if learning_rate < 0 or learning_rate > 1:
+                    raise ValueError
 
-                optimizer = input("what optimizer would you like to use? (SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto)\n")
-                choices=["SGD", "Adam", "Adamax", "AdamW", "NAdam", "RAdam", "RMSProp", "auto"]
-                if optimizer not in choices: raise ValueError
+                optimizer = input(
+                    "what optimizer would you like to use? (SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto)\n"
+                )
+                choices = [
+                    "SGD",
+                    "Adam",
+                    "Adamax",
+                    "AdamW",
+                    "NAdam",
+                    "RAdam",
+                    "RMSProp",
+                    "auto",
+                ]
+                if optimizer not in choices:
+                    raise ValueError
 
-                device = input("what device would you like to use? (0 = gpu, cpu = cpu, mps = apple silicon gpu)\n")
+                device = input(
+                    "what device would you like to use? (0 = gpu, cpu = cpu, mps = apple silicon gpu)\n"
+                )
                 print(device)
-                if device != "0" and device != "cpu" and device != "mps": raise ValueError
+                if device != "0" and device != "cpu" and device != "mps":
+                    raise ValueError
 
                 create.main(epochs, batch_size, learning_rate, optimizer, device)
-            
+
             elif selection == "n":
                 create.main()
 
             else:
                 raise ValueError
-            
+
         elif selection == "2":
             print("this feature is not yet implemented. please try again later.")
             exit()
@@ -58,7 +79,6 @@ try:
 
 
 except Exception as e:
-
     if e == "ValueError":
         print("Invalid input. Please try again.")
         exit()
